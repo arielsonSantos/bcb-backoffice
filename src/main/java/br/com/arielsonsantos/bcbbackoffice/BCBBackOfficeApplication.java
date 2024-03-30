ac227365f595b55971fc0e2e0348eb98cb27d317
arielsonSantos <arielsonelias@gmail.com>
2024-03-30 09:27:58 -0300

    Initial Commit

---
+++ src/main/java/br/com/arielsonsantos/bcbbackoffice/BCBBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package br.com.arielsonsantos.bcbbackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BCBBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BCBBackOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arielsonsantos/bcbbackoffice/BCBBackOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arielsonsantos.bcbbackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCBBackOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
